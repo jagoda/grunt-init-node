b1bea87dbb8b40f7d5807a662b44b204620624d9
Jeff Jagoda <jsjagoda@us.ibm.com>
2013-09-10 14:52:56 -0400

    Initial commit.

---
+++ template.js
@@ -0,0 +1,38 @@
+exports.description = "Creates a basic node project with test infrastructure.";
+exports.notes = "This template creates an initial Gruntfile and package.json.";
+exports.warnOn = [ "Gruntfile.js", "package.json" ];
+
+exports.template = function (grunt, init, done) {
+    
+    init.process(
+        {},
+        [
+            init.prompt("author_name"),
+            init.prompt("author_email"),
+            init.prompt("homepage"),
+            init.prompt("licenses"),
+            init.prompt("name"),
+            init.prompt("npm_test", "grunt"),
+            init.prompt("repository"),
+            init.prompt("version")
+        ],
+        function (error, properties) {
+            var files = init.filesToCopy(properties);
+            
+            init.copyAndProcess(files, properties);
+            init.writePackageJSON("package.json", properties);
+            
+            grunt.util.spawn(
+                {
+                    args : [
+                        "install", "--save-dev", "async", "grunt-cli",
+                        "grunt-contrib-jshint", "istanbul", "jshint", "mocha"
+                    ],
+                    cmd  : "npm",
+                    opts : { stdio: "inherit" }
+                },
+                done
+            );
+        }
+    );
+};
