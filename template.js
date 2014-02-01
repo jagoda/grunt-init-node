86a5b5ccb591863fcc3ff6edf0c12bae3fd5d3a3
Jeff Jagoda <jeffrey.jagoda@gmail.com>
2014-02-01 16:35:19 -0500

    Update style rules.

--- template.js
+++ template.js
@@ -5,49 +5,51 @@ exports.notes = "This template creates an initial Gruntfile and package.json.";
 exports.warnOn = [ "Gruntfile.js", "package.json" ];
 
 exports.template = function (grunt, init, done) {
-    
-    function createDirectory (path) {
-        try {
-            fs.mkdirSync(path);
-        } catch (error) {
-            grunt.fail.warn(error);
-        }
-    }
-    
-    init.process(
-        {},
-        [
-            init.prompt("author_name"),
-            init.prompt("author_email"),
-            init.prompt("homepage"),
-            init.prompt("licenses"),
-            init.prompt("name"),
-            init.prompt("npm_test", "grunt"),
-            init.prompt("repository"),
-            init.prompt("version")
-        ],
-        function (error, properties) {
-            var files = init.filesToCopy(properties);
-            
-            init.addLicenseFiles(files, properties.licenses);
-            init.copyAndProcess(files, properties);
-            init.writePackageJSON("package.json", properties);
-            
-            createDirectory("lib");
-            createDirectory("test");
-            
-            grunt.util.spawn(
-                {
-                    args : [
-                        "install", "--save-dev", "chai", "grunt-cli",
-                        "grunt-contrib-jshint", "grunt-jscs-checker",
-                        "istanbul", "jscs", "jshint", "mocha", "q"
-                    ],
-                    cmd  : "npm",
-                    opts : { stdio: "inherit" }
-                },
-                done
-            );
-        }
-    );
+
+	function createDirectory (path) {
+		try {
+			fs.mkdirSync(path);
+		}
+		catch (error) {
+			grunt.fail.warn(error);
+		}
+	}
+
+	init.process(
+		{},
+		[
+			init.prompt("author_name"),
+			init.prompt("author_email"),
+			init.prompt("homepage"),
+			init.prompt("licenses"),
+			init.prompt("name"),
+			init.prompt("description"),
+			init.prompt("npm_test", "grunt"),
+			init.prompt("repository"),
+			init.prompt("version")
+		],
+		function (error, properties) {
+			var files = init.filesToCopy(properties);
+
+			createDirectory("lib");
+			createDirectory("test");
+
+			init.addLicenseFiles(files, properties.licenses);
+			init.copyAndProcess(files, properties);
+			init.writePackageJSON("package.json", properties);
+
+			grunt.util.spawn(
+				{
+					args : [
+						"install", "--save-dev", "chai", "grunt-cli",
+						"grunt-contrib-jshint", "grunt-jscs-checker",
+						"istanbul", "jscs", "jshint", "mocha"
+					],
+					cmd  : "npm",
+					opts : { stdio : "inherit" }
+				},
+				done
+			);
+		}
+	);
 };
